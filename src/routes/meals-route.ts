23a83582cd10029b2056b91a9eeecd7eb6ad587d
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-17 17:54:57 -0300

    ✅ Middleware intercepta a requisição post

--- src/routes/meals-route.ts
+++ src/routes/meals-route.ts
@@ -1,29 +1,42 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
+import { cookiesValidation } from '../middlewares/cookies-validation'
 
 async function mealsRoute(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
-    const meatsRequestBodySchema = z.object({
-      name: z.string(),
-      description: z.string(),
-      date: z.string(),
-      hour: z.string(),
-      onDiet: z.enum(['yes', 'no']),
-    })
+  app.post(
+    '/',
+    {
+      preHandler: [cookiesValidation],
+    },
+    async (request, reply) => {
+      const meatsRequestBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        hour: z.string(),
+        onDiet: z.enum(['yes', 'no']),
+      })
 
-    const { name, description, date, hour, onDiet } =
-      meatsRequestBodySchema.parse(request.body)
+      const { name, description, date, hour, onDiet } =
+        meatsRequestBodySchema.parse(request.body)
 
-    await knex('meals').insert({
-      id: crypto.randomUUID(),
-      name,
-      description,
-      createdAt: new Date(`${date}T${hour}`).toISOString(),
-      onDiet,
-    })
+      await knex('meals').insert({
+        id: crypto.randomUUID(),
+        name,
+        description,
+        createdAt: new Date(`${date}T${hour}`).toISOString(),
+        onDiet,
+      })
 
-    return reply.status(201).send('')
+      return reply.status(201).send('')
+    },
+  )
+
+  app.get('/', async () => {
+    const meals = await knex('meals').select()
+
+    return { meals }
   })
 }
 
