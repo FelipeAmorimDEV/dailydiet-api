5661c712dd6d4863fe4f254161af3a68f8e4b03c
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-17 16:37:53 -0300

    ✅ Request body validado com zod

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -1,16 +1,28 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
 
 async function usersRoute(app: FastifyInstance) {
   app.post('/', async (request, replay) => {
-    const userRequestBody = request.body
+    const userRequestBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+    })
+
+    const { name, email } = userRequestBodySchema.parse(request.body)
 
     await knex('users').insert({
-      name: userRequestBody.name,
-      email: userRequestBody.email,
+      name,
+      email,
     })
 
     return replay.status(201).send()
+  })
+
+  app.get('/', async () => {
+    const users = await knex('users').select()
+
+    return { users }
   })
 }
 
