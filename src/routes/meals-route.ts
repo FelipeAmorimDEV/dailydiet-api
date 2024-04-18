e26a0748057431531d1a1c3bde3ded311ac276bf
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-18 15:05:05 -0300

    ✅ Add DELETE /refeicao/:id + PUT /refeicao/:id

--- src/routes/meals-route.ts
+++ src/routes/meals-route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-import { z } from 'zod'
+import { string, z } from 'zod'
 import { cookiesValidation } from '../middlewares/cookies-validation'
 
 async function mealsRoute(app: FastifyInstance) {
@@ -81,10 +81,58 @@ async function mealsRoute(app: FastifyInstance) {
         id: z.string().uuid(),
       })
 
+      const { sessionId } = request.user
+      const { id } = requestParamsSchema.parse(request.params)
+      const meal = await knex('meals').where('id', id).first()
+      const isAnAuthorizedUser = meal?.sessionId === sessionId
+
+      if (!isAnAuthorizedUser) {
+        return reply.status(401).send({ error: 'Unouthorized' })
+      }
+
+      await knex('meals').where('id', id).del()
+
+      return reply.status(200).send()
+    },
+  )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [cookiesValidation],
+    },
+    async (request, reply) => {
+      const requestParamsSchema = z.object({
+        id: string().uuid(),
+      })
+
       const { id } = requestParamsSchema.parse(request.params)
       const { sessionId } = request.user
+      const meal = await knex('meals').where('id', id).first()
+      const isAnAuthorizedUser = meal?.sessionId === sessionId
 
-      await knex('meals').where({ sessionId, id }).del()
+      if (!isAnAuthorizedUser) {
+        return reply.status(401).send({ error: 'Unouthorized' })
+      }
+
+      const mealRequestBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        hour: z.string(),
+        onDiet: z.enum(['yes', 'no']),
+      })
+      const { name, description, date, hour, onDiet } =
+        mealRequestBodySchema.parse(request.body)
+
+      await knex('meals')
+        .where('id', id)
+        .update({
+          name,
+          description,
+          createdAt: new Date(`${date}T${hour}`).toISOString(),
+          onDiet,
+        })
 
       return reply.status(200).send()
     },
