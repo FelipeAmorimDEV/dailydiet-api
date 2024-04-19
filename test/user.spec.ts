d487b1bac8d94bd75935c9e28d24ec0dd28e493e
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-19 11:46:53 -0300

    ✅ Teste e2e /users

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -1,10 +1,47 @@
 
-import { describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
+import { app } from '../src/app'
+
 
 
 describe('User route', () => {
-  it('user should be created', () => {
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest --all')
+  })
 
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new user', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'felipe amorim',
+        email: 'felipe@gmail.comm'
+      }).expect(201)
+  })
+
+  it('should be able to validate email existence for new user creation', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'felipe amorim',
+        email: 'felipe@gmail.comm'
+      }).expect(201)
+
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'felipe amorim',
+        email: 'felipe@gmail.comm'
+      }).expect(201)
   })
 })
