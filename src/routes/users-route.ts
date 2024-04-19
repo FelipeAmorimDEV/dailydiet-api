7a9215a44b2a925cf05a0b1916cb8e12d1b9567c
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-19 13:14:04 -0300

    ✅ Refactoring: Cookie criado apenas quando ainda não existe

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -9,11 +9,19 @@ async function usersRoute(app: FastifyInstance) {
       email: z.string(),
     })
 
+
+    let sessionId = request.cookies.sessionID
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      replay.setCookie('sessionID', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 7, // 7 days
+      })
+    }
+
     const { name, email } = userRequestBodySchema.parse(request.body)
-    const sessionId = crypto.randomUUID()
-
     const isExistentEmail = await knex('users').where('email', email)
-
     if (isExistentEmail.length > 0) {
       return replay.status(409).send({ error: 'email already exists' })
     }
@@ -23,11 +31,6 @@ async function usersRoute(app: FastifyInstance) {
       name,
       email,
       sessionId,
-    })
-
-    replay.setCookie('sessionID', sessionId, {
-      path: '/',
-      maxAge: 60 * 60 * 7, // 7 days
     })
 
     return replay.status(201).send()
